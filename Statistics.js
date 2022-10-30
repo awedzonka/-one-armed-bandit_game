3547c386af8bbda10e8add7bcb6b63344178e3e8
awedzonka <aneta.wedzonka@gmail.com>
2022-10-30 14:44:03 +0100

    one-armed-bandit_game_v.2

---
+++ Statistics.js
@@ -0,0 +1,30 @@
+class Statistics {
+    constructor() {
+        this.gameResults = [
+            { win: true, bid: 2 },
+            { win: false, bid: -2 }
+        ]
+    }
+
+    addGameToStatistics (win, bid) {
+        let gameResult = {
+            win,
+            bid
+        }
+        this.gameResults.push(gameResult);
+    console.log(gameResults);
+    }
+    showGameStatistics() {
+
+     let gameNumber= this.gameResults.length;
+     let winNumber= this.gameResults.filter(el=>el.win).length;
+    let loseGame = this.gameResults.filter(el=>!el.win).length;
+     console.log(gameNumber);
+     console.log(winNumber);
+     console.log(loseGame);
+        // return [gameNumber, winNumber, loseGame]
+   
+     
+    }
+}
+const stats = new Statistics();
