6f76163681d6a92ebf3b1d9cc2c650a6a40b132f
awedzonka <aneta.wedzonka@gmail.com>
2022-11-05 15:55:27 +0100

    one-armed-bandit_game_v.5

---
+++ Game.js
@@ -0,0 +1,17 @@
+class Game {
+    constructor() {
+        this.stats = new Statistics();
+        this.wallet = new Wallet(100);
+        document.getElementById("zagraj").addEventListener("click", this.startGame);
+        
+
+    }
+
+    render() {
+
+    }
+    startGame() {
+
+    }
+}
+const game= new Game();
